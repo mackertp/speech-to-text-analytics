7b60d29c115a164bf55e521f66b4a96c6621dde3
Preston Mackert <mackertp@my.canisius.edu>
2018-07-26 10:23:24 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,136 @@
+"""
+Using flask to create a clean looking interface for the speech to text application created
+
+was helped by using the following page to learn about building a flask app
+https://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972
+"""
+
+# ---------------------------------------------------------------------------------------------------- #
+# imports 
+# ---------------------------------------------------------------------------------------------------- #
+
+import os
+import ast
+import json
+from flask import Flask, render_template, request, session
+import helper_functions as helper
+
+# ---------------------------------------------------------------------------------------------------- #
+# main
+# ---------------------------------------------------------------------------------------------------- #
+
+# initialize the app
+app= Flask(__name__)
+
+# the home screen
+@app.route("/")
+def main():
+	return render_template("mainmenu.html")
+
+
+# ---------------------------------------------------------------------------------------------------- #
+# main navigation pages
+# ---------------------------------------------------------------------------------------------------- #
+
+@app.route("/sendfiles")
+def send_files():
+	helper.send_batch("audio_files")
+	return render_template("index.html")
+
+
+# ---------------------------------------------------------------------------------------------------- #
+
+@app.route("/transcripts")
+def transcripts():
+	# set up the selections for 
+	transcriptions = []
+	for json_file in os.listdir(os.getcwd()):
+		if json_file.endswith(".json"):
+			transcriptions.append(json_file)
+
+	return render_template("transcripts.html", transcripts=transcriptions)
+
+
+# ---------------------------------------------------------------------------------------------------- #
+
+@app.route("/analytics/<transcript>")
+def analytics(transcript):
+	"""for each of the different analytic functions, we need to pass the desired outputs, because all 
+	data structures get converted to strings""" 
+
+	# get the dictionary from json data
+	converted_json = helper.convert_json_to_data(transcript)
+
+	# transcript already a string
+	transcript = converted_json.get("transcript")
+
+	# format total confidence
+	percent = round(float(converted_json.get("confidence"))*100, 2)
+
+	# format the word confidence levels
+	word_conf = converted_json.get("words")
+	print(word_conf)
+
+
+	# render the template!
+	return render_template("analytics.html", text=transcript, percent=percent, words=word_conf)
+
+
+# ---------------------------------------------------------------------------------------------------- #
+# analytics pages
+# ---------------------------------------------------------------------------------------------------- #
+
+@app.route("/printtranscript/<text>")
+def print_transcript(text):
+	return render_template("printtranscript.html", text=text)
+
+
+# ---------------------------------------------------------------------------------------------------- #
+
+@app.route("/totalconfidence/<percent>")
+def total_confidence(percent):
+	percent = float(percent)
+	return render_template("totalconfidence.html", percent=percent)
+
+
+# ---------------------------------------------------------------------------------------------------- #
+
+@app.route("/wordconfidence/<words>")
+def word_confidence(words):
+
+	words = ast.literal_eval(words)
+	new_words = []
+	for word in words:
+		percent = round(word[1]*100,2)
+		new_words.append([word[0], percent])
+	print(new_words)
+	return render_template("wordconfidence.html", words=new_words)
+
+
+# ---------------------------------------------------------------------------------------------------- #
+
+@app.route("/analyzetone/<text>")
+def analyze_tone(text):
+	tones = helper.analyze_tone(text)
+	for tone in tones:
+		tones[tone] = float(round(tones[tone]*100,2))
+	return render_template("analyzetone.html", text=tones)
+
+
+# ---------------------------------------------------------------------------------------------------- #
+
+@app.route("/personalityinsights/<text>")
+def personality_menu(text):
+	try:
+		personality = helper.analyze_personality(text)
+		return render_template("personalitymenu.html", personality=personality)
+	except:
+		return render_template("nopersonality.html")
+
+
+# ---------------------------------------------------------------------------------------------------- #
+# call main
+# ---------------------------------------------------------------------------------------------------- #
+
+if __name__ == "__main__":
+	app.run(debug=True)
---
+++ helper_functions.py
@@ -0,0 +1,209 @@
+"""
+All of the functions used to produce data for the menu system
+
+@author Preston Mackert
+"""
+
+# ---------------------------------------------------------------------------------------------------- #
+# imports 
+# ---------------------------------------------------------------------------------------------------- #
+
+import json
+import os
+import ast
+import requests
+from os.path import join, dirname
+from watson_developer_cloud import SpeechToTextV1
+from watson_developer_cloud import ToneAnalyzerV3
+from watson_developer_cloud import PersonalityInsightsV3
+from watson_developer_cloud.tone_analyzer_v3 import ToneInput
+from watson_developer_cloud.websocket import RecognizeCallback
+
+# ---------------------------------------------------------------------------------------------------- #
+# support functions for speech to text
+# ---------------------------------------------------------------------------------------------------- #
+
+def get_transcript(audio_file_name, file_type, folder):
+	""" given an audio file and file type, outputs the file into a json file using IBM watson SDK 
+	https://github.com/watson-developer-cloud/python-sdk/blob/master/examples/speech_to_text_v1.py """
+	speech_to_text = SpeechToTextV1(
+		username="0c91239b-51e8-4aa0-8e41-15e3450128fe",
+		password="m5Zy5gcPC0Kb",
+		url="https://stream.watsonplatform.net/speech-to-text/api")
+
+	# opens the audiofile and gathers the transcipt result with word confidence
+	with open(join(dirname(os.getcwd()+"/"+folder+"/"), audio_file_name), "rb") as audio_file:
+		result = json.dumps(speech_to_text.recognize(audio=audio_file, content_type="audio/"+
+			file_type, timestamps=False, word_confidence=True), indent=2)
+
+	# writes the result into a json file
+	output_filename = audio_file_name.replace(file_type, "json")
+	with open(output_filename, "w") as outfile:
+		json.dump(result, outfile)
+
+
+# ---------------------------------------------------------------------------------------------------- #
+
+def send_batch(folder):
+	""" takes a folder of audio files and converts them all into json transcripts """
+	try:
+		for audio_file in os.listdir(folder):
+			filename, file_extension = os.path.splitext(audio_file)
+			file_extension = file_extension.replace(".", "")
+			print("transcribing "+filename+"."+file_extension+"...")
+			get_transcript(filename+"."+file_extension, file_extension, folder)
+
+	except:
+		print("invalid folder")
+	
+
+# ---------------------------------------------------------------------------------------------------- #
+
+def convert_json_to_data(json_file):
+	""" takes in a json file and reads it into pythonic data, reformated for easier reading """
+	json_data = json.loads(open(json_file).read())
+	formatted_dict = json.loads(json_data)
+	results = formatted_dict.get("results")
+
+	# watson's json response breaks data up into partial dictionaries and needs to be restitched
+	word_confidence_hub = []
+	partial_confidence_hub = []
+	full_transcipt = ""
+	chunks = []
+
+	# commence the stitching
+	for partial_dict in results:
+		# each partial dict has two values. first is the content, second is boolean True
+		alternatives = partial_dict.get("alternatives")[0]
+
+		# gather all of the partial content
+		word_confidence = alternatives.get("word_confidence")
+		partial_confidence = alternatives.get("confidence")
+		partial_transcript = alternatives.get("transcript")
+
+		# stitch partial content into larger datasets
+		partial_confidence_hub.append(partial_confidence)
+		full_transcipt += partial_transcript
+		# the word confidence is nested... unnesting it
+		for word in word_confidence:
+			word_confidence_hub.append(word)
+
+		# add the partial_dict for reference purposes
+		chunks.append(partial_dict)
+
+	# calculating the overall confidence level from partial confidences
+	confidence = sum(partial_confidence_hub) / len(partial_confidence_hub)
+	
+	# reconstructing the dictionary into something more useful and returning
+	return {"transcript":full_transcipt, "confidence":confidence, "words":word_confidence_hub, "chunks":chunks}
+
+
+# ---------------------------------------------------------------------------------------------------- #
+# support functions for tone analysis 
+# ---------------------------------------------------------------------------------------------------- #
+
+def analyze_tone(text):
+	""" given some text, send it to IBM Watson's tone analysis tool 
+	https://github.com/watson-developer-cloud/python-sdk/blob/master/examples/tone_analyzer_v3.py """
+	tone_analyzer = ToneAnalyzerV3(
+		username="c72a34f2-f417-44d0-8f98-f9a363aede59",
+		password="ZHxPQN0ZKlep",
+		url="https://gateway.watsonplatform.net/tone-analyzer/api",
+		version='2017-09-26')
+
+	json_data = json.dumps(tone_analyzer.tone(tone_input=text, content_type="text/plain"), indent=2)
+	formatted_dict = json.loads(json_data)
+	tones = format_tone(formatted_dict)
+	return tones
+
+# ---------------------------------------------------------------------------------------------------- #
+
+def format_tone(data):
+	""" deconstructing the dictionary returned into just the tones and their scores """
+	tone_dict = data["document_tone"]
+	tone_dict = tone_dict["tones"]
+	new_dict = {}
+	for tone in tone_dict:
+		tone_name = tone["tone_name"]
+		tone_score = tone["score"]
+		new_dict[tone_name] = tone_score
+
+	return new_dict
+
+
+# ---------------------------------------------------------------------------------------------------- #
+# support functions for personality insights
+# ---------------------------------------------------------------------------------------------------- #
+
+def analyze_personality(text):
+	""" given some text, send it to IBM Watson's personality insight tool, requires at least 100 words
+	https://github.com/watson-developer-cloud/python-sdk """
+	personality_insights = PersonalityInsightsV3(
+		version="2017-10-13",
+		username="7c0d63c1-6757-41bf-9890-0016638588b4",
+		password="hmHsYX8gqTra",
+		url="https://gateway.watsonplatform.net/personality-insights/api")
+
+	json_data = json.dumps(personality_insights.profile(text, content_type='text/plain', raw_scores=True, 
+		consumption_preferences=True), indent=2)
+	formatted_dict = json.loads(json_data)
+	personality_profile = format_personality(formatted_dict)
+	return personality_profile
+
+
+# ---------------------------------------------------------------------------------------------------- #
+
+def format_personality(formatted_dict):
+	""" taking the entire formmated dictionary from the JSON, deconstruct it into something readable """
+	# these are hardcoded because these are always going to be the keys... unless IBM changes it, then 
+	# check this by printing out the formmated_dict
+	personality = formatted_dict["personality"]
+	needs = formatted_dict["needs"]
+	values = formatted_dict["values"]
+	con_prefs = formatted_dict["consumption_preferences"]
+	warnings = formatted_dict["warnings"]
+
+	personality_dicts = {}
+	needs_dict = {}
+	values_dict = {}
+	con_pref_dict = {}
+
+	# deconstructing the personality
+	for key in personality:
+		name = key["name"]
+		percent = key["percentile"]
+		children = key["children"]
+		reformatted_children = {}
+		for child in children:
+			child_name = child["name"]
+			child_percent = child["percentile"]
+			reformatted_children[child_name] = child_percent
+		personality_dicts[name] = [percent, reformatted_children]
+
+	# deconstructing the needs
+	for key in needs:
+		name = key["name"]
+		percent = key["percentile"]
+		needs_dict[name] = percent
+
+	# deconstructing the values
+	for key in values:
+		name = key["name"]
+		percent = key["percentile"]
+		values_dict[name] = percent
+
+	# deconstructing consumption preferences
+	for key in con_prefs:
+		name = key["name"]
+		prefs = key["consumption_preferences"]
+		pref_dict = {}
+		for key in prefs:
+			pref_name = key["name"]
+			pref_score = key["score"]
+			pref_dict[pref_name] = pref_score
+		con_pref_dict[name] = pref_dict
+
+	# returning easier format
+	formatted_personality = {"personality": personality_dicts, "needs": needs_dict, "values": values_dict, "con_prefs": con_pref_dict}
+	return formatted_personality	
+
